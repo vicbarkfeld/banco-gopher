b6ea136d85b6d2c89f5f36a9efe95484f4917a9f
victoriabarkfeld <vic.barkfeld@hotmail.com>
2022-05-11 15:58:27 -0300

    retorno e pacotes

--- main.go
+++ main.go
@@ -1,46 +1,13 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-		return "Deposito realizado com sucesso", c.saldo
-	} else {
-		return "Valor do deposito menor que zero", c.saldo
-	}
-}
-
-func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
-		c.saldo -= valorDaTransferencia
-		contaDestino.Depositar(valorDaTransferencia)
-		return true
-	} else {
-		return false
-	}
-}
+import (
+	"banco/contas"
+	"fmt"
+)
 
 func main() {
-	contaDaVic := ContaCorrente{titular: "Vic", saldo: 300}
-	contaDaSandra := ContaCorrente{titular: "Sandra", saldo: 100}
+	contaDaVic := contas.ContaCorrente{Titular: "Vic", Saldo: 300}
+	contaDaSandra := contas.ContaCorrente{Titular: "Sandra", Saldo: 100}
 
 	status := contaDaVic.Transferir(-200, &contaDaSandra)
 
