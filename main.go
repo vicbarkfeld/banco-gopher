5b3f44bd9bd25abc7c0aee367328770051184a9b
victoriabarkfeld <vic.barkfeld@hotmail.com>
2022-05-19 21:02:02 -0300

    tipos aninhados alterando visibilidade

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"banco/clientes"
 	"banco/contas"
 	"fmt"
 )
 
 func main() {
-	clienteVic := clientes.Titular{"Vic", "123.123.122.12", "Desenvolvedora Go"}
-	contaDaVic := contas.ContaCorrente{clienteVic, 152, 123552, 1005.5}
-	fmt.Println(contaDaVic)
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
+
+	fmt.Println(contaExemplo.ObterSaldo())
 }
