9d67bf8d1629967b8f10f780fc37c056cc29c000
victoriabarkfeld <vic.barkfeld@hotmail.com>
2022-05-19 21:23:31 -0300

    Criando conta poupança

---
+++ contas/contaPoupanca.go
@@ -0,0 +1,42 @@
+package contas
+
+import "banco/clientes"
+
+type ContaPoupanca struct {
+	Titular                              clientes.Titular
+	NumeroAgencia, Operacao, NumeroConta int
+	saldo                                float64
+}
+
+func (c ContaPoupanca) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "saldo insuficiente"
+	}
+}
+
+func (c ContaPoupanca) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.saldo
+	} else {
+		return "Valor do deposito menor que zero", c.saldo
+	}
+}
+
+func (c ContaPoupanca) Transferir(valorDaTransferencia float64, contaDestino ContaPoupanca) bool {
+	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
+
+func (c ContaPoupanca) ObterSaldo() float64 {
+	return c.saldo
+}
